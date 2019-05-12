7d4f5769a613150e653b9d90d5235cd5cdbff3f7
Daria <DARIAPIERZGALSKA@gmail.com>
2019-05-12 10:03:47 +0200

    Działający switch - polecnie 4 wersja druga zaproponowana przez wykłądowcę switch z boolean

--- src/main/java/Task2/Bookstore.java
+++ src/main/java/Task2/Bookstore.java
@@ -16,28 +16,71 @@ public class Bookstore {
 
         int userChoice = Integer.parseInt(scanner.nextLine());
 
-        while (userChoice != 1 && userChoice != 2) {
+        while (userChoice < 0 && userChoice > 6) {
             userChoice = Integer.parseInt(scanner.nextLine());
         }
 
-        while (userChoice != 2) {
+//        while (userChoice != 2) {
+//            switch (userChoice) {
+//                case 1:
+//                    System.out.println("Wybrałeś: Wyświetlenie listy książek");
+//                    break;
+//                case 2:
+//                    break;
+//                case 3:
+//                    System.out.println("Wybrałeś: Dodanie nowej książki");
+//                    break;
+//                case 4:
+//                    System.out.println("Wybrałeś: Usunięcie książki po nazwie");
+//                    break;
+//                case 5:
+//                    System.out.println("Wybrałeś: Edytowanie roku wydania książki");
+//                    break;
+//                case 6:
+//                    System.out.println("Wybrałeś: Zapisanie listy książek do pliku csv");
+//                    break;
+//            }
+
+        boolean a = true;
+        while (a) {
             switch (userChoice) {
                 case 1:
-                    System.out.println("Wyświetlenie listy książek");
+                    System.out.println("Wybrałeś: Wyświetlenie listy książek");
                     break;
                 case 2:
+                    a = false;
+                    System.out.println("Wybrałeś: Koniec działania aplikacji");
+                    break;
+                case 3:
+                    System.out.println("Wybrałeś: Dodanie nowej książki");
+                    break;
+                case 4:
+                    System.out.println("Wybrałeś: Usunięcie książki po nazwie");
+                    break;
+                case 5:
+                    System.out.println("Wybrałeś: Edytowanie roku wydania książki");
+                    break;
+                case 6:
+                    System.out.println("Wybrałeś: Zapisanie listy książek do pliku csv");
                     break;
             }
-            plikMenu();
-            userChoice = Integer.parseInt(scanner.nextLine());
-        }
-        System.out.println("Koniec działania aplikacji");
+            if (a) {
+                plikMenu();
+                userChoice = Integer.parseInt(scanner.nextLine());
+            }
 
+        }
     }
+
 
     private static void plikMenu() {
         System.out.println("Wpisz:\n " +
                 "1 - aby wyświetlić listę książek\n " +
-                "2 - aby wyjść z aplikacji");
+                "2 - aby wyjść z aplikacji \n " +
+                "3 - aby dodać nową książkę \n " +
+                "4 - aby usunąć książki po nazwie \n " +
+                "5 - aby edytować roku wydania książki \n " +
+                "6 - aby zapisać listę książek do pliku csv \n "
+        );
     }
 }
